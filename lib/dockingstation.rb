af77b3277743f7ad4c85863c024a823f333acaba
tanilserbes <serbestanil@gmail.com>
2020-04-17 16:37:12 +0100

    14 finished

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -1,16 +1,20 @@
 require_relative 'bike'
 
 class DockingStation
-attr_reader :bike
+attr_reader :bikes
+
+  def initialize
+    @bikes = []
+  end
 
   def release_bike
-   raise 'There are no bikes' if !@bike
-   @bike
+   raise 'There are no bikes' if @bikes.empty?
+   @bikes.pop
   end
 
   def dock(bike)
-    raise 'the dock is full' if @bike
-    @bike = bike
+    raise 'the dock is full' if @bikes.length >= 20
+    @bikes << bike
   end
 
 end
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -18,16 +18,16 @@ describe DockingStation do
    it {is_expected.to respond_to(:dock).with(1).argument}
 
   describe "#dock" do
-   
+
    it 'returns docked bikes' do
     bike = Bike.new
     subject.dock(bike)
-    expect(subject.bike).to eq bike
+    expect(subject.bikes).to include bike
    end
-   
+
    context "when the dock is full" do
      it "raises an error 'the dock is full'" do
-       subject.dock(Bike.new)
+       20.times {subject.dock(Bike.new)}
        expect { subject.dock(Bike.new) }.to raise_error('the dock is full')
      end
    end
