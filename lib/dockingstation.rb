7a94bd55b0918df1285161cb7493c9cb4c6f9579
Declan <declankolakowski@gmail.com>
2020-04-17 15:44:34 +0000

    refactored our docking station code to include SRP for checking full and empty status of the object

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -8,13 +8,23 @@ attr_reader :bikes
   end
 
   def release_bike
-   raise 'There are no bikes' if @bikes.empty?
+   raise 'There are no bikes' if empty?
    @bikes.pop
   end
 
   def dock(bike)
-    raise 'the dock is full' if @bikes.length >= 20
+    raise 'the dock is full' if full?
     @bikes << bike
+  end
+  
+  private
+  
+  def full?
+    @bikes.length >= 20
+  end
+  
+  def empty?
+    @bikes.empty?
   end
 
 end
